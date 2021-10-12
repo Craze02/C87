ece894add6eb8412db0510e654f5d004efbce3d8
Craze02 <70636961+Craze02@users.noreply.github.com>
2021-10-12 12:56:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDV2YvDU5MM6cfB73O0B2Re_AZ7KeBaiuo",
+    authDomain: "storytelling-app-8dec3.firebaseapp.com",
+    projectId: "storytelling-app-8dec3",
+    storageBucket: "storytelling-app-8dec3.appspot.com",
+    messagingSenderId: "478562606146",
+    appId: "1:478562606146:web:1a69e9fd2ecd4a9d80a1db"
+  };
